20b22a24c44d996e54ba56ee148dff2621100eff
Orisz <39217182+Orisz@users.noreply.github.com>
2019-12-11 16:11:13 +0200

    run scripts for rishon
    to run them on rishon just type in 'rishon' terminal:
    sbatch -c 2 --gres=gpu:1 -o slurm-test.out -J all_exp run_exps_script.sh

---
+++ hw2/run_all_exp.py
@@ -0,0 +1,11 @@
+import torch
+import hw2.experiments as experiments
+
+
+if __name__ == '__main__':
+    print("Ori & Amir exp")
+    experiments.experiment1_1()
+    experiments.experiment1_2()
+    experiments.experiment1_3()
+    experiments.experiment1_4()
+    experiments.experiment2()
---
+++ hw2/run_exps_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate cs236781-hw
+
+python run_all_exp.py
